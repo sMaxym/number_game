608c6e3d762ed1a7aa37897806a1f65dff8ee782
lurak <lurak@ukr.net>
2018-10-18 18:12:01 +0300

    Function which return prime numbers

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-print("Hello World!!!")
-def summ(x, y):
-    return x + y
-def product(a,b):
-    prd = a * b
-    return prd
+# print("Hello World!!!")
+# def summ(x, y):
+#     return x + y
+# def product(a,b):
+#     prd = a * b
+#     return prd
+
+n = int(input())
+count = 0
+for i in range(n):
+    for j in range(n):
+        count += 1
+print(count)
