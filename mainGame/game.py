32001be5b449d81fae67ae73860865873f1e2bb4
Voravomas <samovarov@ucu.edu.ua>
2018-10-23 00:05:42 +0300

    Game is almost done!

--- mainGame/game.py
+++ mainGame/game.py
@@ -2,98 +2,169 @@ import pygame
 pygame.init()
 win = pygame.display.set_mode((1000, 550))
 pygame.display.set_caption("Space Settlements")
+start = True
+Mercury = False
+Venus = False
+Earth = False
+Mars = False
+Jupiter = False
+Saturn = False
+Uranus = False
+run = False
+gameOver = False
 
+#--------Do not touch! Constants-------#
+width = 80
+height = 80
+radius = 200
+zoneRadX = int(width / 2)
+zoneRadY = int(height / 2)
+startPic = pygame.image.load("pictures/main/start.png")
+youWin = pygame.image.load("pictures/main/youWin.png")
+gameOver = pygame.image.load("pictures/main/gameOver.png")
+#-------Do not touch! Constants--------#
+#-----Buttons-----#
 
+begin = pygame.transform.scale(pygame.image.load("pictures/main/begin.png"), (215, 100))
+begin_r = begin.get_rect()
+beginOver = pygame.transform.scale(pygame.image.load("pictures/main/beginOver.png"), (215, 100))
+beginClicked = pygame.transform.scale(pygame.image.load("pictures/main/beginClicked.png"), (215, 100))
+
+#-----Buttons-----#
+#-------Functions--------#
+
+def createCraters(picLoc, list, picLocCol, values, width, height, radius):
+    mainList = []
+    for item in range(len(list)):
+        craterObj = pygame.transform.scale(pygame.image.load(picLoc), (width, height))
+        mainList.append([list[item], craterObj, craterObj.get_rect(), \
+         pygame.transform.scale(pygame.image.load(picLocCol), (width, height)), \
+          values[item], False, radius, zoneColor])
+    return mainList
+
+def drawWindow(background):
+    win.blit((pygame.image.load(background)), (0, 0))
+    pygame.display.update()
 
 def text_objects(text, font):
     textSurface = font.render(text, True, (0, 0, 0))
     return textSurface, textSurface.get_rect()
 
-width = 50
-height = 50
-radius = 200
-zoneRadX = int(width / 2)
-zoneRadY = int(height / 2)
-def createCraters(list, values, width, height, radius):
-    mainList = []
-    for item in range(len(list)):
-        craterObj = pygame.transform.scale(pygame.image.load("crater.png"), (width, height))
-        mainList.append([list[item], craterObj, craterObj.get_rect(), \
-         pygame.transform.scale(pygame.image.load("craterColonised.png"), (width, height)), \
-          values[item], False, radius, True])
-    return mainList
+#-------Functions--------#
 
+#-----Start-----#
 
-
-craterValue = [2, 5, 7, 8, 13]
-mainList = createCraters(((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), craterValue, width, height, radius)
-
-
-# def createCraters(list):
-#     global craterValue
-#     global mainList
-#     for item in range(len(list)):
-#         mainList.append((list[item], "crater.png", craterValue, "craterColonised.png", False, 150))
-
-
-
-# for item in range(len(mainList)):
-#     crater1 = pygame.transform.scale(pygame.image.load(), (150, 150))
-#     crater_r = crater1.get_rect()
-#     crater1Colon = pygame.transform.scale(pygame.image.load("craterColonised.png"), (150, 150))
-
-
-
-
-
-
-
-# craterXCord = [300]
-# craterYCord = [125]
-
-def drawWindow():
-    win.blit((pygame.image.load("bg.png")), (0, 0))
-    pygame.display.update()
-
-
-isClicked = False
-
-run = True
-
-while run:
+while start:
     pygame.time.delay(100)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    drawWindow()
-
 
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            start = False
+    win.blit(startPic, (0, 0))
 
-    # if isClicked:
-    #     pygame.draw.circle(win, (11, 142, 0), (craterXCord[0] + 75, craterYCord[0] + 75), 150, 4)
+    begin_r.x = 393
+    begin_r.y = 400
+    if begin_r.collidepoint(mouse):
+        win.blit(beginOver, (393, 400))
+        if click[0] == 1:
+            win.blit(beginClicked, (393, 400))
+            start = False
+            run = True
+            Mercury = True
+    else:
+        win.blit(begin, (393, 400))
+
+    pygame.display.update()
+
+#-----Start-----#
+
+#-------Variables for each level--------#
+
+
+if Mercury:
+    craterValue = [2, 5, 7, 8, 13]
+    zoneColor = (11, 142, 0)
+    craterLoc = "pictures/Mercury/crater.png"
+    craterLocCol = "pictures/Mercury/craterColonised.png"
+    backgr = "pictures/Mercury/bg1.png"
+    mainList = createCraters(craterLoc, \
+     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
+      craterLocCol, craterValue, width, height, radius)
+elif Venus:
+    craterValue = [2, 5, 7, 8, 13]
+    zoneColor = (11, 142, 0)
+    craterLoc = "pictures/Venus/crater.png"
+    craterLocCol = "pictures/Venus/craterColonised.png"
+    backgr = "pictures/Venus/bg1.png"
+    mainList = createCraters(craterLoc, \
+     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
+      craterLocCol, craterValue, width, height, radius)
+elif Earth:
+    craterValue = [2, 5, 7, 8, 13]
+    zoneColor = (11, 142, 0)
+    craterLoc = "pictures/Earth/crater.png"
+    craterLocCol = "pictures/Earth/craterColonised.png"
+    backgr = "pictures/Earth/bg1.png"
+    mainList = createCraters(craterLoc, \
+     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
+      craterLocCol, craterValue, width, height, radius)
+elif Mars:
+    craterValue = [2, 5, 7, 8, 13]
+    zoneColor = (11, 142, 0)
+    craterLoc = "pictures/Mars/crater.png"
+    craterLocCol = "pictures/Mars/craterColonised.png"
+    backgr = "pictures/Mars/bg1.png"
+    mainList = createCraters(craterLoc, \
+     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
+      craterLocCol, craterValue, width, height, radius)
+elif Jupiter:
+    craterValue = [2, 5, 7, 8, 13]
+    zoneColor = (11, 142, 0)
+    craterLoc = "pictures/Jupiter/crater.png"
+    craterLocCol = "pictures/Jupiter/craterColonised.png"
+    backgr = "pictures/Jupiter/bg1.png"
+    mainList = createCraters(craterLoc, \
+     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
+      craterLocCol, craterValue, width, height, radius)
+elif Saturn:
+    craterValue = [2, 5, 7, 8, 13]
+    zoneColor = (11, 142, 0)
+    craterLoc = "pictures/Saturn/crater.png"
+    craterLocCol = "pictures/Saturn/craterColonised.png"
+    backgr = "pictures/Saturn/bg1.png"
+    mainList = createCraters(craterLoc, \
+     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
+      craterLocCol, craterValue, width, height, radius)
+elif Uranus:
+    craterValue = [2, 5, 7, 8, 13]
+    zoneColor = (11, 142, 0)
+    craterLoc = "pictures/Uranus/crater.png"
+    craterLocCol = "pictures/Uranus/craterColonised.png"
+    backgr = "pictures/Uranus/bg1.png"
+    mainList = createCraters(craterLoc, \
+     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
+      craterLocCol, craterValue, width, height, radius)
+
+#-------Variables for each level--------#
+
+
+
+#-----Game-----#
+
+while run:
+    pygame.time.delay(100)
+    drawWindow(backgr)
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     for item in range(len(mainList)):
         win.blit(mainList[item][1], mainList[item][0])
-
-
-
-
-
-
-
-
-    # for element in range(len(mainList)):
-    #     if mainList[element][2].collidepoint(mouse):
-    #         pygame.draw.circle(win, (11, 142, 0), ((mainList[element][0][0] + 75), \
-    #          (mainList[element][0][1] + 75)), mainList[element][6], 4)
-    #         win.blit(mainList[item][1], mainList[item][0])
-    #         (pygame.transform.scale(pygame.image.load("crater.png").get_rect(), (150, 150)).get_rect()).x = mainList[element][0][0]
-    #         (pygame.transform.scale(pygame.image.load("crater.png").get_rect(), (150, 150)).get_rect()).y = mainList[element][0][1]
-    #     if clic
 
     for element in mainList:
         if element[2].collidepoint(mouse):
@@ -104,48 +175,21 @@ while run:
             if click[0] == 1:
                 element[1] = element[3]
                 element[5] = True
-                element[7] = False
         else:
             win.blit(element[1], (element[0][0], element[0][1]))
             element[2].x = element[0][0]
             element[2].y = element[0][1]
         if element[5]:
             pygame.draw.circle(win, (11, 142, 0), (element[0][0] + zoneRadX, element[0][1] + zoneRadY), radius, 4)
-        if element[7]:
-            smallText = pygame.font.Font("freesansbold.ttf", 15)
-            textSurf, textRect = text_objects(str(element[4]), smallText)
-            textRect.center = element[0]
-            win.blit(textSurf, textRect)
-
-
-
-
-    # if crater_r.collidepoint(mouse):
-    #     pygame.draw.circle(win, (11, 142, 0), (craterXCord[0] + 75, craterYCord[0] + 75), 150, 4)
-    #     win.blit(crater1, (craterXCord[0], craterYCord[0]))
-    #     crater_r.x = craterXCord[0]
-    #     crater_r.y = craterYCord[0]
-    #     if click[0] == 1:
-    #         crater1 = crater1Colon
-    #         isClicked = True
-    # else:
-    #     win.blit(crater1, (craterXCord[0], craterYCord[0]))
-    #     crater_r.x = craterXCord[0]
-    #     crater_r.y = craterYCord[0]
-
-
-
-
-
-
-
-
-
-
-
+        smallText = pygame.font.Font("freesansbold.ttf", 15)
+        textSurf, textRect = text_objects(str(element[4]), smallText)
+        textRect.center = element[0]
+        win.blit(textSurf, textRect)
     
     pygame.display.update()
 pygame.quit()
+
+#-----End-----#
 
     # win.blit(craterOver1, (craterXCord[0] - 75, craterYCord[0] - 75))
 
@@ -173,3 +217,58 @@ pygame.quit()
     #     win.blit(crater1, (craterXCord[0], craterYCord[0]))
     #     crater_r.x = craterXCord[0]
     #     crater_r.y = craterYCord[0]
+
+
+
+# def createCraters(list):
+#     global craterValue
+#     global mainList
+#     for item in range(len(list)):
+#         mainList.append((list[item], "crater.png", craterValue, "craterColonised.png", False, 150))
+
+
+
+# for item in range(len(mainList)):
+#     crater1 = pygame.transform.scale(pygame.image.load(), (150, 150))
+#     crater_r = crater1.get_rect()
+#     crater1Colon = pygame.transform.scale(pygame.image.load("craterColonised.png"), (150, 150))
+
+
+
+
+
+
+
+# craterXCord = [300]
+# craterYCord = [125]
+
+
+
+
+
+    # if crater_r.collidepoint(mouse):
+    #     pygame.draw.circle(win, (11, 142, 0), (craterXCord[0] + 75, craterYCord[0] + 75), 150, 4)
+    #     win.blit(crater1, (craterXCord[0], craterYCord[0]))
+    #     crater_r.x = craterXCord[0]
+    #     crater_r.y = craterYCord[0]
+    #     if click[0] == 1:
+    #         crater1 = crater1Colon
+    #         isClicked = True
+    # else:
+    #     win.blit(crater1, (craterXCord[0], craterYCord[0]))
+    #     crater_r.x = craterXCord[0]
+    #     crater_r.y = craterYCord[0]
+
+
+    # for element in range(len(mainList)):
+    #     if mainList[element][2].collidepoint(mouse):
+    #         pygame.draw.circle(win, (11, 142, 0), ((mainList[element][0][0] + 75), \
+    #          (mainList[element][0][1] + 75)), mainList[element][6], 4)
+    #         win.blit(mainList[item][1], mainList[item][0])
+    #         (pygame.transform.scale(pygame.image.load("crater.png").get_rect(), (150, 150)).get_rect()).x = mainList[element][0][0]
+    #         (pygame.transform.scale(pygame.image.load("crater.png").get_rect(), (150, 150)).get_rect()).y = mainList[element][0][1]
+    #     if clic
+
+    
+    # if isClicked:
+    #     pygame.draw.circle(win, (11, 142, 0), (craterXCord[0] + 75, craterYCord[0] + 75), 150, 4)
