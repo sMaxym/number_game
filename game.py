8b80b60f74de2f3aa9d1be55a1d33d477a36241f
lurak <lurak@ukr.net>
2018-10-23 17:23:12 +0300

    Created level switch

--- game.py
+++ game.py
@@ -84,6 +84,7 @@ def generateCondition(ulam = 0, prime = 1, happy = 2):
     chooseCond = random.randint(condMin, condMax)
     return chooseCond
 
+
 #-------Functions--------#
 
 #-----Start-----#
@@ -116,204 +117,212 @@ while start:
 #-----Start-----#
 
 #-------Variables for each level--------#
+for planet in range(7):
+    currentTurn = PLAYER
+    currentCondition = generateCondition()
+    firstPlayerCoord = None
+    playerStartTime = None
+    currentIter = 1
+    playerIter = 1
+    planetCraterValue = []
+    planetCraterValue.extend(numberGenerators.generateRandomPrime(2, 100))
+    planetCraterValue.extend(numberGenerators.generateRandomUlam(2, 100))
+    planetCraterValue.extend(numberGenerators.generateRandomHappy(1, 100))
+    planetCoords = []
+    # planetCoords = [(200, 200),(500, 100), (500, 300), (310, 360), (300, 100)]
+    planetRadiuses = {}
+    planetValues = {}
+    planetOwner = {}
 
-currentTurn = PLAYER
-currentCondition = generateCondition()
-firstPlayerCoord = None
-playerStartTime = None
-currentIter = 1
-playerIter = 1
-mercuryCraterValue = []
-mercuryCraterValue.extend(numberGenerators.generateRandomPrime(2, 100))
-mercuryCraterValue.extend(numberGenerators.generateRandomUlam(2, 100))
-mercuryCraterValue.extend(numberGenerators.generateRandomHappy(1, 100))
-mercuryCoords = [(200, 200),(500, 100), (500, 300), (310, 360), (300, 100)]
-mercuryRadiuses = {}
-mercuryValues = {}
-mercuryOwner = {}
-for index in range(0, len(mercuryCoords)):
-    mercuryRadiuses[mercuryCoords[index]] = radius
-    mercuryValues[mercuryCoords[index]] = mercuryCraterValue[index]
+    if planet == 0:
+        # craterValue = [2, 5, 7, 8, 13]
+        zoneColor = (11, 142, 0)
+        planetCoords = [(200, 200),(500, 100), (500, 300), (310, 360), (300, 100)]
+        craterLoc = "pictures/Mercury/crater.png"
+        craterLocCol = "pictures/Mercury/craterColonised.png"
+        backgr = "pictures/Mercury/bg1.png"
+        mainList = createCraters(craterLoc, \
+         planetCoords, \
+          craterLocCol, planetCraterValue, width, height, radius) 
+    elif planet == 1:
+        planetCoords = [(100, 100),(500, 100), (500, 300), (310, 360), (300, 100)]
+        zoneColor = (11, 142, 0)
+        craterLoc = "pictures/Venus/crater.png"
+        craterLocCol = "pictures/Venus/craterColonised.png"
+        backgr = "pictures/Venus/bg1.png"
+        mainList = createCraters(craterLoc, \
+         planetCoords, \
+          craterLocCol, planetCraterValue, width, height, radius) 
+    elif planet == 2:
+        planetCoords = [(100, 100),(500, 100), (500, 300), (310, 360), (300, 100)]
+        zoneColor = (11, 142, 0)
+        craterLoc = "pictures/Earth/crater.png"
+        craterLocCol = "pictures/Earth/craterColonised.png"
+        backgr = "pictures/Earth/bg1.png"
+        mainList = createCraters(craterLoc, \
+         planetCoords, \
+          craterLocCol, planetCraterValue, width, height, radius) 
+    elif planet == 3:
+        planetCoords = [(100, 100),(500, 100), (500, 300), (310, 360), (300, 100)]
+        zoneColor = (11, 142, 0)
+        craterLoc = "pictures/Mars/crater.png"
+        craterLocCol = "pictures/Mars/craterColonised.png"
+        backgr = "pictures/Mars/bg1.png"
+        mainList = createCraters(craterLoc, \
+         planetCoords, \
+          craterLocCol, planetCraterValue, width, height, radius) 
+    elif planet == 4:
+        planetCoords = [(100, 100),(500, 100), (500, 300), (310, 360), (300, 100)]
+        zoneColor = (11, 142, 0)
+        craterLoc = "pictures/Jupiter/crater.png"
+        craterLocCol = "pictures/Jupiter/craterColonised.png"
+        backgr = "pictures/Jupiter/bg1.png"
+        mainList = createCraters(craterLoc, \
+         planetCoords, \
+          craterLocCol, planetCraterValue, width, height, radius) 
+    elif planet == 5:
+        planetCoords = [(100, 100),(500, 100), (500, 300), (310, 360), (300, 100)]
+        zoneColor = (11, 142, 0)
+        craterLoc = "pictures/Saturn/crater.png"
+        craterLocCol = "pictures/Saturn/craterColonised.png"
+        backgr = "pictures/Saturn/bg1.png"
+        mainList = createCraters(craterLoc, \
+         planetCoords, \
+          craterLocCol, planetCraterValue, width, height, radius) 
+    elif planet == 6:
+        planetCoords = [(100, 100),(500, 100), (500, 300), (310, 360), (300, 100)]
+        zoneColor = (11, 142, 0)
+        craterLoc = "pictures/Uranus/crater.png"
+        craterLocCol = "pictures/Uranus/craterColonised.png"
+        backgr = "pictures/Uranus/bg1.png"
+        mainList = createCraters(craterLoc, \
+         planetCoords, \
+          craterLocCol, planetCraterValue, width, height, radius) 
 
-if Mercury:
-    zoneColor = (11, 142, 0)
-    craterLoc = "pictures/Mercury/crater.png"
-    craterLocCol = "pictures/Mercury/craterColonised.png"
-    backgr = "pictures/Mercury/bg1.png"
-    mainList = createCraters(craterLoc, \
-     mercuryCoords, \
-      craterLocCol, mercuryCraterValue, width, height, radius)
-elif Venus:
-    craterValue = [2, 5, 7, 8, 13]
-    zoneColor = (11, 142, 0)
-    craterLoc = "pictures/Venus/crater.png"
-    craterLocCol = "pictures/Venus/craterColonised.png"
-    backgr = "pictures/Venus/bg1.png"
-    mainList = createCraters(craterLoc, \
-     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
-      craterLocCol, craterValue, width, height, radius)
-elif Earth:
-    craterValue = [2, 5, 7, 8, 13]
-    zoneColor = (11, 142, 0)
-    craterLoc = "pictures/Earth/crater.png"
-    craterLocCol = "pictures/Earth/craterColonised.png"
-    backgr = "pictures/Earth/bg1.png"
-    mainList = createCraters(craterLoc, \
-     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
-      craterLocCol, craterValue, width, height, radius)
-elif Mars:
-    craterValue = [2, 5, 7, 8, 13]
-    zoneColor = (11, 142, 0)
-    craterLoc = "pictures/Mars/crater.png"
-    craterLocCol = "pictures/Mars/craterColonised.png"
-    backgr = "pictures/Mars/bg1.png"
-    mainList = createCraters(craterLoc, \
-     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
-      craterLocCol, craterValue, width, height, radius)
-elif Jupiter:
-    craterValue = [2, 5, 7, 8, 13]
-    zoneColor = (11, 142, 0)
-    craterLoc = "pictures/Jupiter/crater.png"
-    craterLocCol = "pictures/Jupiter/craterColonised.png"
-    backgr = "pictures/Jupiter/bg1.png"
-    mainList = createCraters(craterLoc, \
-     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
-      craterLocCol, craterValue, width, height, radius)
-elif Saturn:
-    craterValue = [2, 5, 7, 8, 13]
-    zoneColor = (11, 142, 0)
-    craterLoc = "pictures/Saturn/crater.png"
-    craterLocCol = "pictures/Saturn/craterColonised.png"
-    backgr = "pictures/Saturn/bg1.png"
-    mainList = createCraters(craterLoc, \
-     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
-      craterLocCol, craterValue, width, height, radius)
-elif Uranus:
-    craterValue = [2, 5, 7, 8, 13]
-    zoneColor = (11, 142, 0)
-    craterLoc = "pictures/Uranus/crater.png"
-    craterLocCol = "pictures/Uranus/craterColonised.png"
-    backgr = "pictures/Uranus/bg1.png"
-    mainList = createCraters(craterLoc, \
-     ((200, 200),(500, 100), (500, 300), (310, 360), (300, 100)), \
-      craterLocCol, craterValue, width, height, radius)
+    for index in range(0, len(planetCoords)):
+        planetRadiuses[planetCoords[index]] = radius
+        planetValues[planetCoords[index]] = planetCraterValue[index]
 
-#-------Variables for each level--------#
+    #-------Variables for each level--------#
 
-if Mercury:
-    win.blit(textMercury, (0, 0))
-elif Venus:
-    win.blit(textVenus, (0, 0))
-elif Earth:
-    win.blit(textEarth, (0, 0))
-elif Mars:
-    win.blit(textMars, (0, 0))
-elif Jupiter:
-    win.blit(textJupiter, (0, 0))
-elif Saturn:
-    win.blit(textSaturn, (0, 0))
-elif Uranus:
-    win.blit(textUranus, (0, 0))
+    if planet == 0:
+        win.blit(textMercury, (0, 0))
+    elif planet == 1:
+        win.blit(textVenus, (0, 0))
+    elif planet == 2:
+        win.blit(textEarth, (0, 0))
+    elif planet == 3:
+        win.blit(textMars, (0, 0))
+    elif planet == 4:
+        win.blit(textJupiter, (0, 0))
+    elif planet == 5:
+        win.blit(textSaturn, (0, 0))
+    elif planet == 6:
+        win.blit(textUranus, (0, 0))
 
-#-----Game-----#
-playerStartTime = time.time()
-while run:
-    print(currentCondition, mercuryOwner)
-    if time.time() - start_time > 5:
-        pygame.time.delay(100)
-        drawWindow(backgr)
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+    #-----Game-----#
+    playerStartTime = time.time()
+    emptySpot = None
+    while run:
+        if time.time() - start_time > 5:
+            pygame.time.delay(100)
+            drawWindow(backgr)
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
 
-        if time.time() - playerStartTime > 5:
-            playerStartTime = time.time()
-            currentIter += 1
-            currentTurn = AI if currentTurn == PLAYER else PLAYER
+            if time.time() - playerStartTime > 5:
+                playerStartTime = time.time()
+                currentIter += 1
+                currentTurn = AI if currentTurn == PLAYER else PLAYER
 
-        for item in range(len(mainList)):
-            win.blit(mainList[item][1], mainList[item][0])
+            for item in range(len(mainList)):
+                win.blit(mainList[item][1], mainList[item][0])
+            emptySpot = False
+            for element in mainList:
+                if not element[5]:
+                    emptySpot = True
+                if element[2].collidepoint(mouse):
+                    pygame.draw.circle(win, friendlyColor, (element[0][0] + zoneRadX, element[0][1] + zoneRadY), radius, 4)
+                    win.blit(element[1], (element[0][0], element[0][1]))
+                    element[2].x = element[0][0]
+                    element[2].y = element[0][1]
+                    if click[0] == 1:
+                        playerPoints = ai.associatedElements(planetOwner, PLAYER, planetCoords)
+                        chosenNumberProps = []
+                        if ulamNumber.ulamNumbers(planetValues[element[0]]):
+                            chosenNumberProps.append(CONDITION_ULAM)
+                        if primeNumber.primeNumber(planetValues[element[0]]):
+                            chosenNumberProps.append(CONDITION_PRIME)
+                        if happyNumbers.happyNumbers(planetValues[element[0]]):
+                            chosenNumberProps.append(CONDITION_HAPPY)
+                        if not element[5] and currentTurn == PLAYER and (playerIter == 1 or ai.distanceAvailability(element[0], planetRadiuses[element[0]], playerPoints)) and currentCondition in chosenNumberProps:
+                            if currentIter == 1:
+                                firstPlayerCoord = element[0]
+                            element[1] = element[3]
+                            element[5] = True
+                            planetOwner[element[0]] = PLAYER
+                            currentTurn = AI
+                            currentIter += 1
+                            playerIter += 1
+                            playerStartTime = time.time()
+                else:
+                    win.blit(element[1], (element[0][0], element[0][1]))
+                    element[2].x = element[0][0]
+                    element[2].y = element[0][1]
+                if element[5]:
+                    curColor = None
+                    if planetOwner[element[0]] == PLAYER:
+                        curColor = friendlyColor
+                    elif planetOwner[element[0]] == AI:
+                        curColor = enemyColor
+                    pygame.draw.circle(win, curColor, (element[0][0] + zoneRadX, element[0][1] + zoneRadY), radius, 4)
+                smallText = pygame.font.Font("freesansbold.ttf", 15)
+                textSurf, textRect = text_objects(str(element[4]), smallText)
+                textRect.center = element[0]
+                win.blit(textSurf, textRect)
 
-        for element in mainList:
-            if element[2].collidepoint(mouse):
-                pygame.draw.circle(win, friendlyColor, (element[0][0] + zoneRadX, element[0][1] + zoneRadY), radius, 4)
-                win.blit(element[1], (element[0][0], element[0][1]))
-                element[2].x = element[0][0]
-                element[2].y = element[0][1]
-                if click[0] == 1:
-                    playerPoints = ai.associatedElements(mercuryOwner, PLAYER, mercuryCoords)
+            if currentTurn == AI:
+                aiTurn = None
+                aiCoordsToChoose = []
+                for coord in planetCoords:
                     chosenNumberProps = []
-                    if ulamNumber.ulamNumbers(mercuryValues[element[0]]):
+                    if ulamNumber.ulamNumbers(planetValues[coord]):
                         chosenNumberProps.append(CONDITION_ULAM)
-                    if primeNumber.primeNumber(mercuryValues[element[0]]):
+                    if primeNumber.primeNumber(planetValues[coord]):
                         chosenNumberProps.append(CONDITION_PRIME)
-                    if happyNumbers.happyNumbers(mercuryValues[element[0]]):
+                    if happyNumbers.happyNumbers(planetValues[coord]):
                         chosenNumberProps.append(CONDITION_HAPPY)
-                    if not element[5] and currentTurn == PLAYER and (playerIter == 1 or ai.distanceAvailability(element[0], mercuryRadiuses[element[0]], playerPoints)) and currentCondition in chosenNumberProps:
-                        if currentIter == 1:
-                            firstPlayerCoord = element[0]
+                    if currentCondition in chosenNumberProps:
+                        aiCoordsToChoose.append(coord)
+                if currentIter != 2:
+                    aiTurn = ai.minimax(aiCoordsToChoose, planetRadiuses, planetValues, planetOwner, 5)
+                    aiTurn = aiTurn[1]
+                    planetOwner[aiTurn] = AI
+                    currentCondition = generateCondition()
+                    
+                    currentTurn = PLAYER
+                    playerStartTime = time.time()
+                else:
+                    aiTurn = ai.generateFirstStep(aiCoordsToChoose, planetRadiuses, planetValues, firstPlayerCoord)
+                    aiTurn = aiTurn[1]
+                    planetOwner[aiTurn] = AI
+                    currentCondition = generateCondition()
+
+                    currentTurn = PLAYER
+                    playerStartTime = time.time()
+                for element in mainList:
+                    if element[0] == aiTurn:
                         element[1] = element[3]
                         element[5] = True
-                        mercuryOwner[element[0]] = PLAYER
-                        currentTurn = AI
-                        currentIter += 1
-                        playerIter += 1
-                        playerStartTime = time.time()
-            else:
-                win.blit(element[1], (element[0][0], element[0][1]))
-                element[2].x = element[0][0]
-                element[2].y = element[0][1]
-            if element[5]:
-                curColor = None
-                if mercuryOwner[element[0]] == PLAYER:
-                    curColor = friendlyColor
-                elif mercuryOwner[element[0]] == AI:
-                    curColor = enemyColor
-                pygame.draw.circle(win, curColor, (element[0][0] + zoneRadX, element[0][1] + zoneRadY), radius, 4)
-            smallText = pygame.font.Font("freesansbold.ttf", 15)
-            textSurf, textRect = text_objects(str(element[4]), smallText)
-            textRect.center = element[0]
-            win.blit(textSurf, textRect)
-
-        if currentTurn == AI:
-            aiTurn = None
-            aiCoordsToChoose = []
-            for coord in mercuryCoords:
-                chosenNumberProps = []
-                if ulamNumber.ulamNumbers(mercuryValues[coord]):
-                    chosenNumberProps.append(CONDITION_ULAM)
-                if primeNumber.primeNumber(mercuryValues[coord]):
-                    chosenNumberProps.append(CONDITION_PRIME)
-                if happyNumbers.happyNumbers(mercuryValues[coord]):
-                    chosenNumberProps.append(CONDITION_HAPPY)
-                if currentCondition in chosenNumberProps:
-                    aiCoordsToChoose.append(coord)
-            if currentIter != 2:
-                aiTurn = ai.minimax(aiCoordsToChoose, mercuryRadiuses, mercuryValues, mercuryOwner, 5)
-                aiTurn = aiTurn[1]
-                mercuryOwner[aiTurn] = AI
-                currentCondition = generateCondition()
-                
-                currentTurn = PLAYER
-                playerStartTime = time.time()
-            else:
-                aiTurn = ai.generateFirstStep(aiCoordsToChoose, mercuryRadiuses, mercuryValues, firstPlayerCoord)
-                aiTurn = aiTurn[1]
-                mercuryOwner[aiTurn] = AI
-                currentCondition = generateCondition()
-
-                currentTurn = PLAYER
-                playerStartTime = time.time()
-            for element in mainList:
-                if element[0] == aiTurn:
-                    element[1] = element[3]
-                    element[5] = True
-             
-    else:
-        playerStartTime = time.time()
-    pygame.display.update()
+            if not emptySpot:
+                break  
+        else:
+            playerStartTime = time.time()
+        print(playerStartTime - time.time())
+        pygame.display.update()
 pygame.quit()
 
 #-----End-----#
